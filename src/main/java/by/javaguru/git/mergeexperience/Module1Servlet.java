6e22501d1cee5f9e144b9aa54d50da9f8371b457
valeryia <dybovik.val@gmail.com>
2024-03-01 17:27:13 +0300

    change files

--- src/main/java/by/javaguru/git/mergeexperience/Module1Servlet.java
+++ src/main/java/by/javaguru/git/mergeexperience/Module1Servlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.annotation.*;
 @WebServlet("/module1")
 public class Module1Servlet extends HttpServlet {
     private String message;
+
     public void init() {
         message = "Модуль 1";
     }
@@ -22,15 +23,34 @@ public class Module1Servlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
 
         out.println("<table>");
-        for (Module1Topics value : Module1Topics.values())
+        for (Module1Topics value : Module1Topics.values()) {
             out.println("<tr><td>"
                     + value.getOrder() + "</td><td>"
                     + value.getTopic() + "</td><td>"
+                    + "<span id='description-" + value.getOrder() + "' style='display: none;'>"
                     + value.getDesc()
-                    + "</td><td><a href=''>Подробнее</a>"
+                    + "</span></td><td><a href='#' onclick='showDescription(" + value.getOrder() + ")'>Подробнее</a>"
                     + "</td></tr>");
+        }
 
         out.println("</table>");
+        out.println(" <button>\n" +
+                "   <img src=\"left.png\">\n" +
+                "  </button>\n" +
+                "  <button>\n" +
+                "    <img src=\"right.png\"" +
+                "  </button>");
+
+        out.println("<script>");
+        out.println("function showDescription(order) {");
+        out.println("    var description = document.getElementById('description-' + order);");
+        out.println("    if (description.style.display === 'none') {");
+        out.println("        description.style.display = 'block';");
+        out.println("    } else {");
+        out.println("        description.style.display = 'none';");
+        out.println("    }");
+        out.println("}");
+        out.println("</script>");
 
         out.println("</body></html>");
     }
